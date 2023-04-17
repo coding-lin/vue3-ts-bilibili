df843765111f8bad36a57b8989389b33aba6f46f
hjl <2764881569@qq.com>
2023-04-17 17:26:05 +0800

    feat:视频列表

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -5,10 +5,13 @@
   <HomeChannel />
   <!-- 轮播图组件 -->
   <HomeSwipe />
+  <!-- 视频列表 -->
+  <HomeVideoList />
 </template>
 
 <script setup lang="ts">
 import AppHeader from '@/components/app-header.vue'
 import HomeChannel from './components/home-channel.vue'
 import HomeSwipe from './components/home-swipe.vue'
+import HomeVideoList from './components/home-video-list.vue'
 </script>
