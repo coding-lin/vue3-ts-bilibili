6154bb1bcde6200bcf93a0cba07e509fa78c63b2
hjl <2764881569@qq.com>
2023-04-17 14:41:04 +0800

    feat:引入Vant组件

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,12 @@ import router from './router'
 import '@/assets/styles/base.less'
 import '@/assets/styles/iconfont.less'
 
-createApp(App).use(router).mount('#app')
+import { Tab, Tabs } from 'vant'
+
+const app = createApp(App)
+// 注册 Vant 组件
+app.use(Tab)
+app.use(Tabs)
+
+app.use(router)
+app.mount('#app')
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,7 +1,11 @@
 <template>
+  <!-- 头部组件 -->
   <AppHeader />
+  <!-- 频道组件 -->
+  <HomeChannel />
 </template>
 
 <script setup lang="ts">
 import AppHeader from '@/components/app-header.vue'
+import HomeChannel from './components/home-channel.vue'
 </script>
