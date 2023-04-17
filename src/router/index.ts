9d81de9a4f611fb4ece12d75317155a37c9b849f
hjl <2764881569@qq.com>
2023-04-17 13:51:14 +0800

    项目路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,14 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 
 const routes: Array<RouteRecordRaw> = [
-  
+  {
+    path: '/',
+    component: () => import('@/views/Home/index.vue')
+  },
+  {
+    path: '/video/:id',
+    component: () => import('@/views/Video/index.vue')
+  }
 ]
 
 const router = createRouter({
---
+++ src/views/Home/index.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>bilibili 主页</h1>
+  <router-link to="/video/1">点我去视频详情页</router-link>
+</template>
